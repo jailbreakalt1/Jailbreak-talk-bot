7fb6f30117dc2815386780208d2530cdabe16603
jailbreakalt1 <ndinikani@gmail.com>
2025-06-27 13:22:21 +0200

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { JailbreakInterface } from "@/components/jailbreak-interface"
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-black">
+      <JailbreakInterface />
+    </div>
+  )
+}
